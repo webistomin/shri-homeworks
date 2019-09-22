6c38ff307f977c5923fc94294fec467bced01ed4
webistomin <webistomin@gmail.com>
2019-09-22 09:10:14 +0300

    Правки

--- 02-html/gulp/tasks/dev/csscomb.js
+++ 02-html/gulp/tasks/dev/csscomb.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const csscomb = require('gulp-csscomb');
 
 gulp.task('csscomb', () => {
-  return gulp.src('src/sass/blocks/*.sass')
+  return gulp.src('./src/sass/blocks/*.sass')
     .pipe(csscomb(require('../../../.csscomb.json')))
-    .pipe(gulp.dest('src/sass/blocks'));
+    .pipe(gulp.dest('./src/sass/blocks'));
 });
