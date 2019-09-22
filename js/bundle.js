50c629796d74998df5742041afd7e771feaf5d41
webistomin <webistomin@gmail.com>
2019-09-22 21:35:14 +0300

    Update 2019-09-22T18:35:11.248Z

--- js/bundle.js
+++ js/bundle.js
@@ -5,16 +5,12 @@ const { body } = document;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 window.addEventListener('resize', () => {
-
   if (window.innerWidth > window.innerHeight
     || Math.abs(lastHeight - window.innerHeight) > 200) {
-
     vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
     lastHeight = window.innerHeight;
-
   }
-
 }, { passive: true });
 
 const detectMob = () => !!(navigator.userAgent.match(/Android/i)
@@ -37,67 +33,41 @@ const branchSelectButton = document.getElementById('js-branch-select-btn');
 const branchSelectPopup = document.getElementById('js-branch-select-popup');
 
 const openPopup = (popupEl) => {
-
   popupEl.classList.toggle('popup_visible');
   if (isMobile) {
-
     disableBodyScroll();
-
   }
-
 };
 
 const closePopup = (popupEl) => {
-
   if (popupEl.classList.contains('popup_visible')) {
-
     popupEl.classList.remove('popup_visible');
     if (isMobile) {
-
       enableBodyScroll();
-
     }
-
   }
-
 };
 
 if (repositorySelectButton) {
-
   repositorySelectButton.addEventListener('click', () => {
-
     openPopup(repositorySelectPopup);
-
   });
-
 }
 
 if (repositorySelectPopup) {
-
   repositorySelectPopup.addEventListener('click', (e) => {
-
     closePopup(e.target);
-
   });
-
 }
 
 if (branchSelectButton) {
-
   branchSelectButton.addEventListener('click', () => {
-
     openPopup(branchSelectPopup);
-
   });
-
 }
 
 if (branchSelectPopup) {
-
   branchSelectPopup.addEventListener('click', (e) => {
-
     closePopup(e.target);
-
   });
-
 }
