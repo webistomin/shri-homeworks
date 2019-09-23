9c31e5394a94a7415774c5627cddd56a70713137
webistomin <webistomin@gmail.com>
2019-09-23 11:29:41 +0300

    Добавил страницу, для теста ие11

--- 03-js/test/promise.spec.js
+++ 03-js/test/promise.spec.js
@@ -3,7 +3,7 @@ var PromisePolyfill = require('../lib/promise-polyfill');
 var should = require('should');
 
 describe('Проверка полифила для промисов:', () => {
-  it('Корректно обрабатывает цепочку', async() => {
+  it('Корректно обрабатывает цепочку', async () => {
     let testValue = '';
     
     await new PromisePolyfill((resolve) => {
@@ -22,5 +22,22 @@ describe('Проверка полифила для промисов:', () => {
       });
     
     testValue.should.equal(137);
+  });
+  
+  it('Корректно обрабатывает гонку промисов', async () => {
+    let testValue = '';
+    
+    const p1 = new PromisePolyfill((resolve) => {
+      setTimeout(resolve, 500, 'Первый');
+    });
+    const p2 = new PromisePolyfill((resolve) => {
+      setTimeout(resolve, 100, 'Второй');
+    });
+    
+    await PromisePolyfill.race([p1, p2]).then((value) => {
+      testValue = value;
+    });
+    
+    testValue.should.equal("Второй");
   });
 });
