8ecaea584983cd3903afc26f3e56d411d0a2acc0
webistomin <webistomin@gmail.com>
2019-09-25 08:22:05 +0300

    Правки вывода

--- 01-nodejs/src/handlers/getAllRepoFiles.js
+++ 01-nodejs/src/handlers/getAllRepoFiles.js
@@ -3,8 +3,8 @@ const getAllFilesInsideFolder = require('../utils/getAllFilesInsideFolder');
 module.exports = getAllRepoFiles = async (directoryPath) => {
   try {
     let result = await getAllFilesInsideFolder(directoryPath);
-    return { repos: result }
+    return result
   } catch (error) {
-    return { message: error }
+    return message
   }
 };
