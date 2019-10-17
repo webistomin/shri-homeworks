34c1266e4735a7f5a18e4486e199a319a97aae5b
webistomin <webistomin@gmail.com>
2019-10-17 12:59:10 +0300

    Updates

--- main.js
+++ main.js
@@ -7,35 +7,14 @@ $(document).ready(function(){
 
 document.addEventListener("DOMContentLoaded", function () {
   const buttonsContainer = document.querySelector(".buttons-wrap");
+  const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
   const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
-  setTimeout(function() {
-    const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
-    const purchaseListContainer = document.createElement('div');
-    const purchaseListTitle = document.createElement('p');
-    const purchaseList = document.createElement('ol');
-    const purchaseListItemOne = document.createElement('li');
-    const purchaseListItemTwo = document.createElement('li');
-    
-    purchaseListContainer.setAttribute('class', 'purchase-list-wrap');
-    purchaseListTitle.setAttribute('class', 'card-description card-description_big description_critical');
-    purchaseListTitle.textContent = 'Список покупок:';
-    purchaseList.setAttribute('class', 'purchase-list');
-    purchaseListItemOne.setAttribute('class', 'purchase-list__item');
-    purchaseListItemOne.textContent = 'Хлеб';
-    purchaseListItemTwo.setAttribute('class', 'purchase-list__item');
-    purchaseListItemTwo.textContent = 'Молоко';
-    
-    purchaseListContainer.appendChild(purchaseListTitle);
-    purchaseListContainer.appendChild(purchaseList);
-    purchaseList.appendChild(purchaseListItemOne);
-    purchaseList.appendChild(purchaseListItemTwo);
-    
-    confirmPurchaseButton.onclick = () => {
-      fridgeInfoContainer.replaceWith(purchaseListContainer);
-      buttonsContainer.style.display = "none";
-    }
-  }, 500);
+  const purchaseListContainer = `<div class="purchase-list-wrap"><p class="card-description card-description_big description_critical">Список покупок:</p><ol class="purchase-list"><li class="purchase-list__item">Хлеб</li><li class="purchase-list__item">Молоко</li></ol></div>`;
   
+  confirmPurchaseButton.onclick = () => {
+    fridgeInfoContainer.replaceWith(purchaseListContainer);
+    buttonsContainer.style.display = "none";
+  };
   
   document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click", function () {
     document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
