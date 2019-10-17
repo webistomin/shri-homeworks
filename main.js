90285654f770f5ecd9d3045a0ef2a53a55c19acc
webistomin <webistomin@gmail.com>
2019-10-17 13:04:44 +0300

    Updates

--- main.js
+++ main.js
@@ -7,14 +7,17 @@ $(document).ready(function(){
 
 document.addEventListener("DOMContentLoaded", function () {
   const buttonsContainer = document.querySelector(".buttons-wrap");
-  const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
   const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
-  const purchaseListContainer = `<div class="purchase-list-wrap"><p class="card-description card-description_big description_critical">Список покупок:</p><ol class="purchase-list"><li class="purchase-list__item">Хлеб</li><li class="purchase-list__item">Молоко</li></ol></div>`;
+    const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
+    const purchaseListContainer = document.createElement('div');
+    purchaseListContainer.classList.add('purchase-list-wrap');
+    purchaseListContainer.innerHTML = `<p class="card-description card-description_big description_critical">Список покупок:</p><ol class="purchase-list"><li class="purchase-list__item">Хлеб</li><li class="purchase-list__item">Молоко</li></ol>`;
+    
+    confirmPurchaseButton.onclick = () => {
+      fridgeInfoContainer.replaceWith(purchaseListContainer);
+      buttonsContainer.style.display = "none";
+    };
   
-  confirmPurchaseButton.onclick = () => {
-    fridgeInfoContainer.replaceWith(purchaseListContainer);
-    buttonsContainer.style.display = "none";
-  };
   
   document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click", function () {
     document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
