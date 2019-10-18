6e6b0ae7d5bd34ddaacf7cc77d17656850062851
webistomin <webistomin@gmail.com>
2019-10-18 18:29:31 +0300

    Updates

--- js/multimedia.js
+++ js/multimedia.js
@@ -1,10 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   const t = document.querySelector('.buttons-wrap'),
-    e = document.querySelector('#cc .card-description');
-    const n = document.querySelector('.buttons-wrap .button_yellow'), c = document.createElement('div'),
-      s = document.createElement('p'), o = document.createElement('ol'), i = document.createElement('li'),
-      a = document.createElement('li');
-    c.setAttribute('class', 'purchase-list-wrap'), s.setAttribute('class', 'card-description card-description_big description_critical'), s.textContent = 'Список покупок:', o.setAttribute('class', 'purchase-list'), i.setAttribute('class', 'purchase-list__item'), i.textContent = 'Хлеб', a.setAttribute('class', 'purchase-list__item'), a.textContent = 'Молоко', c.appendChild(s), c.appendChild(o), o.appendChild(i), o.appendChild(a), n.onclick = (() => {
+        e = document.querySelector('#cc .card-description');
+  
+  const n = document.querySelector('.buttons-wrap .button_yellow'),
+        c = document.createElement('div'),
+        s = document.createElement('p'),
+        o = document.createElement('ol'),
+        i = document.createElement('li'),
+        a = document.createElement('li');
+  
+    c.setAttribute('class', 'purchase-list-wrap');
+    c.innerHTML = `<p class="card-description card-description_big description_critical">
+                    Список покупок:</p>
+                    <ol class="purchase-list">
+                        <li class="purchase-list__item">Хлеб</li>
+                        <li class="purchase-list__item">Молоко</li>
+                    </ol>`;
+    n.onclick = (() => {
       e.replaceWith(c), t.style.display = 'none';
     });
  document.getElementsByClassName('header-menu__switcher')[0].addEventListener('click', () => {
