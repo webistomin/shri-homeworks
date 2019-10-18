723d0eff9586ac13534e3310680577e16e6585fe
webistomin <webistomin@gmail.com>
2019-10-18 10:39:11 +0300

    633

--- 10-Performance/gulpfile.js
+++ 10-Performance/gulpfile.js
@@ -1,7 +1,13 @@
 const gulp = require('gulp');
 const ghPages = require('gh-pages');
 const path = require('path');
+const fixmyjs = require('gulp-fixmyjs');
 
 gulp.task('deploy', (done) => {
   ghPages.publish(path.join(process.cwd(), './src'), done);
 });
+
+gulp.task('fixjs', () => gulp.src('./src/js/main.js')
+  .pipe(fixmyjs({}))
+  .pipe(gulp.dest("./src"))
+);
